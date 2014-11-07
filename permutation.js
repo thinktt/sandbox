7f4d86bf74640578a59d69c8699ab1dc4b2f2579
thinktt <github@thinktt.e4ward.com>
2014-11-06 23:54:20 -0600

    update

--- permutation.js
+++ permutation.js
@@ -1,4 +1,4 @@
-var arr = [1,2,3,4];
+var arr = [1,2, 3];
 
 function getPermutation(list) {
 	var arr = list.slice();
@@ -7,28 +7,31 @@ function getPermutation(list) {
 	var curent; 
 	var i, j;
 
+	console.log('Howdy');
 	if(list.length === 1) return [arr];
 
 	for(i=0; i<list.length; i++) { 
 		current = list[i];
 		arr = list.slice();
 		arr.splice(i,1);
-		//console.log(current);
-		//console.log(arr);
+		console.log(current);
+		console.log(arr);
 
 		permutations = getPermutation(arr); 
+		console.log('Permutations ', permutations);
 		for(j=0; j<permutations.length; j++) {
 			permutations[j].unshift(current);
 			permsList.push(permutations[j]);
 		}
 	}
-	
+	console.log('Permslist ', permsList);
 	return permsList;
 
 }
 
 
-console.log(getPermutation(arr));
+//console.log(getPermutation(arr));
+getPermutation(arr);
 
 
 function getNextPerms () {
